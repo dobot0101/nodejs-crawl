f21502b8ce0f4593d62c80506f11a296c9ab3a73
dobot <lhl890@naver.com>
2022-02-05 01:39:04 +0900

    import 문제 수정, lowdb 연습

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,3 @@
-"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -8,42 +7,69 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-Object.defineProperty(exports, "__esModule", { value: true });
-// ES6 or TypeScript:
-const cheerio = require("cheerio");
-const axios_1 = require("axios");
-const iconv = require("iconv-lite");
-// const iconv = require('iconv-lite');
 // import { join, dirname } from 'path';
-const lowdb_1 = require("lowdb");
+import axios from 'axios';
+import * as cheerio from 'cheerio';
+import iconv from 'iconv-lite';
+import { Low, JSONFile } from 'lowdb';
 // import { fileURLToPath } from 'url';
 // const __dirname = dirname(fileURLToPath(import.meta.url));
 // Use JSON file for storage
 // const file = join(__dirname, 'db.json');
 const file = './db.json';
-const adapter = new lowdb_1.JSONFile(file);
-const db = new lowdb_1.Low(adapter);
-// Read data from JSON file, this will set db.data content
-db.read().then(() => {
-    // If file.json doesn't exist, db.data will be null
-    // Set default data
-    db.data || (db.data = { posts: [] });
-    // db.data = db.data || { posts: [] } // for node < v15.x
-    // Create and query items using plain JS
-    db.data.posts.push('hello world');
-    db.data.posts[0];
-    // You can also use this syntax if you prefer
-    const { posts } = db.data;
-    posts.push('hello world');
-    // Write db.data content to db.json
-    db.write().then(() => {
-        console.log('db write complete.');
+const adapter = new JSONFile(file);
+const db = new Low(adapter);
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const html = yield getHtml();
+        if (html && html.data) {
+            // 네이버 뉴스 페이지의 경우 한글이 깨져서 아래와 같이 iconv 사용
+            const decodedStr = iconv.decode(html.data, 'euc-kr');
+            // const $ = cheerio.load(iconv.decode(html.data, 'euc-kr').toString());
+            const $ = cheerio.load(decodedStr);
+            const articleList = $('ul.newsList').children('li.block1');
+            const articles = [];
+            articleList.each((i, el) => {
+                let title = '';
+                if ($(el).find('dd.articleSubject').length > 0) {
+                    title = $(el).find('dd.articleSubject > a').text().trim();
+                }
+                else if ($(el).find('dt.articleSubject').length > 0) {
+                    title = $(el).find('dt.articleSubject > a').text().trim();
+                }
+                // const contents = $(el).find('dd.articleSummary').contents();
+                // console.log(contents);
+                articles.push({
+                    title,
+                    body: $(el)
+                        .find('dd.articleSummary')
+                        .contents()
+                        .filter(function () {
+                        return this.type === 'text';
+                    })
+                        .text()
+                        .trim(),
+                    company: $(el).find('dd.articleSummary > span.press').text().trim(),
+                    writeDate: $(el).find('dd.articleSummary > span.wdate').text().trim(),
+                });
+            });
+            // console.log(articles);
+            yield db.read();
+            // console.log(result);
+            db.data = db.data || { articles: [] };
+            db.data.articles = articles;
+            yield db.write();
+            const result = db.data.articles.filter(article => article.company === '아시아경제');
+            db.data.articles = result;
+            yield db.write();
+        }
     });
-});
+}
+main();
 function getHtml() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            return yield axios_1.default.get('https://finance.naver.com/news/mainnews.naver', {
+            return yield axios.get('https://finance.naver.com/news/mainnews.naver', {
                 responseType: 'arraybuffer',
                 responseEncoding: 'binary',
             });
@@ -53,66 +79,3 @@ function getHtml() {
         }
     });
 }
-// const html = await getHtml();
-// if (html && html.data) {
-//   // 네이버 뉴스 페이지의 경우 한글이 깨져서 아래와 같이 iconv 사용
-//   const decodedStr = iconv.decode(html.data, 'euc-kr');
-//   // const $ = cheerio.load(iconv.decode(html.data, 'euc-kr').toString());
-//   const $ = cheerio.load(decodedStr);
-//   const articleList = $('ul.newsList').children('li.block1');
-//   const articles: { title: string; body: string; company: string; writeDate: string }[] = [];
-//   articleList.each(function (i, el) {
-//     let title = '';
-//     if ($(this).find('dd.articleSubject').length > 0) {
-//       title = $(this).find('dd.articleSubject > a').text().trim();
-//     } else if ($(this).find('dt.articleSubject').length > 0) {
-//       title = $(this).find('dt.articleSubject > a').text().trim();
-//     }
-//     articles.push({
-//       title,
-//       body: $(this)
-//         .find('dd.articleSummary')
-//         .contents()
-//         .filter(function () {
-//           return this.type === 'text';
-//         })
-//         .text()
-//         .trim(),
-//       company: $(this).find('dd.articleSummary > span.press').text().trim(),
-//       writeDate: $(this).find('dd.articleSummary > span.wdate').text().trim(),
-//     });
-//   });
-//   console.log(articles);
-// }
-getHtml().then(html => {
-    if (html && html.data) {
-        // 네이버 뉴스 페이지의 경우 한글이 깨져서 아래와 같이 iconv 사용
-        // iconv.decode(html.data, 'euc-kr').toString();
-        const $ = cheerio.load(iconv.decode(html.data, 'euc-kr').toString());
-        const articleList = $('ul.newsList').children('li.block1');
-        const articles = [];
-        articleList.each(function (i, el) {
-            let title = '';
-            if ($(this).find('dd.articleSubject').length > 0) {
-                title = $(this).find('dd.articleSubject > a').text().trim();
-            }
-            else if ($(this).find('dt.articleSubject').length > 0) {
-                title = $(this).find('dt.articleSubject > a').text().trim();
-            }
-            articles.push({
-                title,
-                body: $(this)
-                    .find('dd.articleSummary')
-                    .contents()
-                    .filter(function () {
-                    return this.type === 'text';
-                })
-                    .text()
-                    .trim(),
-                company: $(this).find('dd.articleSummary > span.press').text().trim(),
-                writeDate: $(this).find('dd.articleSummary > span.wdate').text().trim(),
-            });
-        });
-        console.log(articles);
-    }
-});
--- index.ts
+++ index.ts
@@ -1,9 +1,7 @@
-// ES6 or TypeScript:
-import * as cheerio from 'cheerio';
-import axios from 'axios';
-import * as iconv from 'iconv-lite';
-// const iconv = require('iconv-lite');
 // import { join, dirname } from 'path';
+import axios from 'axios';
+import * as cheerio from 'cheerio';
+import iconv from 'iconv-lite';
 import { Low, JSONFile } from 'lowdb';
 // import { fileURLToPath } from 'url';
 
@@ -11,34 +9,64 @@ import { Low, JSONFile } from 'lowdb';
 
 // Use JSON file for storage
 // const file = join(__dirname, 'db.json');
-const file = './db.json';
 
 type Data = {
-  posts: string[];
+  articles: Articles; // Expect posts to be an array of strings
 };
+
+type Articles = { title: string; body: string; company: string; writeDate: string }[];
+
+const file = './db.json';
 const adapter = new JSONFile<Data>(file);
 const db = new Low<Data>(adapter);
 
-// Read data from JSON file, this will set db.data content
-db.read().then(() => {
-  // If file.json doesn't exist, db.data will be null
-  // Set default data
-  db.data ||= { posts: [] };
-  // db.data = db.data || { posts: [] } // for node < v15.x
+async function main() {
+  const html = await getHtml();
+  if (html && html.data) {
+    // 네이버 뉴스 페이지의 경우 한글이 깨져서 아래와 같이 iconv 사용
+    const decodedStr = iconv.decode(html.data, 'euc-kr');
+    // const $ = cheerio.load(iconv.decode(html.data, 'euc-kr').toString());
+    const $ = cheerio.load(decodedStr);
+    const articleList = $('ul.newsList').children('li.block1');
+    const articles: Articles = [];
 
-  // Create and query items using plain JS
-  db.data.posts.push('hello world');
-  db.data.posts[0];
+    articleList.each((i: any, el: any) => {
+      let title = '';
+      if ($(el).find('dd.articleSubject').length > 0) {
+        title = $(el).find('dd.articleSubject > a').text().trim();
+      } else if ($(el).find('dt.articleSubject').length > 0) {
+        title = $(el).find('dt.articleSubject > a').text().trim();
+      }
 
-  // You can also use this syntax if you prefer
-  const { posts } = db.data;
-  posts.push('hello world');
+      articles.push({
+        title,
+        body: $(el)
+          .find('dd.articleSummary')
+          .contents()
+          .filter(function () {
+            return this.type === 'text';
+          })
+          .text()
+          .trim(),
+        company: $(el).find('dd.articleSummary > span.press').text().trim(),
+        writeDate: $(el).find('dd.articleSummary > span.wdate').text().trim(),
+      });
+    });
 
-  // Write db.data content to db.json
-  db.write().then(() => {
-    console.log('db write complete.');
-  });
-});
+    await db.read();
+
+    db.data = db.data || { articles: [] };
+    db.data.articles = articles;
+
+    await db.write();
+
+    // const result = db.data.articles.filter(article => article.company === '아시아경제');
+    // db.data.articles = result;
+    // await db.write();
+  }
+}
+
+main();
 
 async function getHtml() {
   try {
@@ -50,73 +78,3 @@ async function getHtml() {
     console.error(error);
   }
 }
-
-// const html = await getHtml();
-// if (html && html.data) {
-//   // 네이버 뉴스 페이지의 경우 한글이 깨져서 아래와 같이 iconv 사용
-//   const decodedStr = iconv.decode(html.data, 'euc-kr');
-//   // const $ = cheerio.load(iconv.decode(html.data, 'euc-kr').toString());
-//   const $ = cheerio.load(decodedStr);
-//   const articleList = $('ul.newsList').children('li.block1');
-//   const articles: { title: string; body: string; company: string; writeDate: string }[] = [];
-
-//   articleList.each(function (i, el) {
-//     let title = '';
-//     if ($(this).find('dd.articleSubject').length > 0) {
-//       title = $(this).find('dd.articleSubject > a').text().trim();
-//     } else if ($(this).find('dt.articleSubject').length > 0) {
-//       title = $(this).find('dt.articleSubject > a').text().trim();
-//     }
-
-//     articles.push({
-//       title,
-//       body: $(this)
-//         .find('dd.articleSummary')
-//         .contents()
-//         .filter(function () {
-//           return this.type === 'text';
-//         })
-//         .text()
-//         .trim(),
-//       company: $(this).find('dd.articleSummary > span.press').text().trim(),
-//       writeDate: $(this).find('dd.articleSummary > span.wdate').text().trim(),
-//     });
-//   });
-
-//   console.log(articles);
-// }
-
-getHtml().then(html => {
-  if (html && html.data) {
-    // 네이버 뉴스 페이지의 경우 한글이 깨져서 아래와 같이 iconv 사용
-    // iconv.decode(html.data, 'euc-kr').toString();
-    const $ = cheerio.load(iconv.decode(html.data, 'euc-kr').toString());
-    const articleList = $('ul.newsList').children('li.block1');
-    const articles: { title: string; body: string; company: string; writeDate: string }[] = [];
-
-    articleList.each(function (i, el) {
-      let title = '';
-      if ($(this).find('dd.articleSubject').length > 0) {
-        title = $(this).find('dd.articleSubject > a').text().trim();
-      } else if ($(this).find('dt.articleSubject').length > 0) {
-        title = $(this).find('dt.articleSubject > a').text().trim();
-      }
-
-      articles.push({
-        title,
-        body: $(this)
-          .find('dd.articleSummary')
-          .contents()
-          .filter(function () {
-            return this.type === 'text';
-          })
-          .text()
-          .trim(),
-        company: $(this).find('dd.articleSummary > span.press').text().trim(),
-        writeDate: $(this).find('dd.articleSummary > span.wdate').text().trim(),
-      });
-    });
-
-    console.log(articles);
-  }
-});
